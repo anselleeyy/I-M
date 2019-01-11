0bd4b6a8345a02130e291c80bdcbdfa5996669c4
anselleeyy <573459407@qq.com>
2019-01-11 14:02:18 +0800

    upload infiniti-common component

---
+++ infiniti-common/src/main/java/cn/ltysyn/infiniti/common/utils/ReturnCode.java
@@ -0,0 +1,33 @@
+package cn.ltysyn.infiniti.common.utils;
+
+public enum ReturnCode {
+	
+	ARTIST_INFO_GOT(10000, "歌手信息获取成功");
+	
+	private int code;
+	
+	private String message;
+	
+	private ReturnCode(int code, String message) {
+		// TODO Auto-generated constructor stub
+		this.code = code;
+		this.message = message;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
