5d0ef2c7f079acc37edb0f4aa836469c4d1ab486
anselleeyy <573459407@qq.com>
2019-01-29 19:37:11 +0800

    修复 bug

--- infiniti-music/src/main/java/cn/ltysyn/infiniti/music/InfinitiMusicApplication.java
+++ infiniti-music/src/main/java/cn/ltysyn/infiniti/music/InfinitiMusicApplication.java
@@ -2,8 +2,10 @@ package cn.ltysyn.infiniti.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InfinitiMusicApplication {
 
 	public static void main(String[] args) {
--- infiniti-music/src/test/java/cn/ltysyn/infiniti/music/InfinitiMusicApplicationTests.java
+++ infiniti-music/src/test/java/cn/ltysyn/infiniti/music/InfinitiMusicApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class InfinitiMongoApplicationTests {
+public class InfinitiMusicApplicationTests {
 	
 	@Test
 	public void contextLoads() {
