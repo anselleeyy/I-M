854ff27fec758f1278cbe36730cab4c7f0acc30d
anselleeyy <573459407@qq.com>
2019-01-29 19:34:37 +0800

    新增服务网关，spring-cloud-gateway

---
+++ infiniti-api-gateway/src/main/java/cn/ltysyn/infiniti/gateway/filter/AccessFilter.java
@@ -0,0 +1,83 @@
+package cn.ltysyn.infiniti.gateway.filter;
+
+import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.server.ServerWebExchange;
+
+import cn.ltysyn.infiniti.common.redis.config.RedisOps;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AccessFilter implements GlobalFilter, Ordered {
+	
+	// url 匹配器
+	private AntPathMatcher pathMatcher = new AntPathMatcher();
+	
+	@Autowired
+	private RedisOps redisOps;
+	
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return -500;
+	}
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		String accessToken = extractToken(exchange.getRequest());
+		
+		if (pathMatcher.match("/**/v2/api-docs/**", exchange.getRequest().getPath().value())) {
+			return chain.filter(exchange);
+		}
+		
+		if (!pathMatcher.match("/api-auth/**", exchange.getRequest().getPath().value())) {
+			if (accessToken == null) {
+				exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+				return exchange.getResponse().setComplete();
+			} else {
+				try {
+					// redis 认证
+					String params = redisOps.get("token:" + accessToken);
+					if (params == null || "".equals(params)) {
+						exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+						return exchange.getResponse().setComplete();
+					}
+				} catch (Exception e) {
+					// TODO: handle exception
+					exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+					return exchange.getResponse().setComplete();
+				}
+			}
+		}
+		
+		return chain.filter(exchange);
+	}
+	
+	private String extractToken(ServerHttpRequest request) {
+		List<String> strings = request.getHeaders().get("Authorization");
+		String authToken = null;
+		if (strings != null) {
+			authToken = strings.get(0).substring("Bearer".length()).trim();
+		}
+		
+		if (StringUtils.isBlank(authToken)) {
+			strings = request.getQueryParams().get("access_token");
+			if (strings != null) {
+				authToken = strings.get(0);
+			}
+		}
+		
+		return authToken;
+	}
+
+}
---
+++ infiniti-api-gateway/src/test/java/cn/ltysyn/infiniti/gateway/InfinitiApiGatewayApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.ltysyn.infiniti.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfinitiApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
