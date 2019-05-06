c96f33a56bf45e134014be9fc9091729657484a2
anselleeyy <573459407@qq.com>
2019-05-06 12:37:03 +0800

    修改网关拦截器多余文件

--- gateway-center/src/main/java/cn/ltysyn/inmusic/filter/AccessFilter.java
+++ gateway-center/src/main/java/cn/ltysyn/inmusic/filter/AccessFilter.java
@@ -19,35 +19,33 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class AccessFilter implements GlobalFilter, Ordered {
-	
-	// url匹配器
-	private AntPathMatcher pathMatcher = new AntPathMatcher();
-	
-	@Autowired
-	private StringRedisTemplate redisTemplate;
 
-	@Override
-	public int getOrder() {
-		// TODO Auto-generated method stub
-		return -500;
-	}
+    // url匹配器
+    private AntPathMatcher pathMatcher = new AntPathMatcher();
 
-	@Override
-	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
-		String accessToken = extractToken(exchange.getRequest());
-		
-		String path = exchange.getRequest().getPath().value();
-		
-		if (pathMatcher.match(UrlPattern.SWAGGER, path)
-				|| pathMatcher.match(UrlPattern.MUSIC_CENTER, path)
-				|| pathMatcher.match(UrlPattern.USER_REGISTER, path)
-				|| pathMatcher.match(UrlPattern.USER_LOGIN, path)
-				|| pathMatcher.match(UrlPattern.ADMIN_LOGIN, path)) {
-			return chain.filter(exchange);
-		}
-		
-		if (accessToken == null) {
+    @Override
+    public int getOrder() {
+        // TODO Auto-generated method stub
+        return -500;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String accessToken = extractToken(exchange.getRequest());
+
+        String path = exchange.getRequest().getPath().value();
+
+        if (pathMatcher.match(UrlPattern.SWAGGER, path) || pathMatcher.match(UrlPattern.MUSIC_CENTER, path)
+            || pathMatcher.match(UrlPattern.USER_REGISTER, path) || pathMatcher.match(UrlPattern.USER_LOGIN, path)
+            || pathMatcher.match(UrlPattern.ADMIN_LOGIN, path)) {
+            return chain.filter(exchange);
+        }
+
+        if (accessToken == null) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         } else {
@@ -57,31 +55,31 @@ public class AccessFilter implements GlobalFilter, Ordered {
                     exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                     return exchange.getResponse().setComplete();
                 }
-            } catch (Exception e) { 
+            } catch (Exception e) {
                 exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                 return exchange.getResponse().setComplete();
             }
         }
-		
-		return chain.filter(exchange);
 
-	}
-	
-	protected String extractToken(ServerHttpRequest	request) {
-		List<String> strings = request.getHeaders().get("Authorization");
-		String authToken = null;
-		if (strings != null) {
-			authToken = strings.get(0).trim();
-		}
-		
-		if (StringUtils.isBlank(authToken)) {
-			strings = request.getHeaders().get("access_token");
-			if (strings != null) {
-				authToken = strings.get(0);
-			}
-		}
-		
-		return authToken;
-	}
-	
+        return chain.filter(exchange);
+
+    }
+
+    private String extractToken(ServerHttpRequest request) {
+        List<String> strings = request.getHeaders().get("Authorization");
+        String authToken = null;
+        if (strings != null) {
+            authToken = strings.get(0).trim();
+        }
+
+        if (StringUtils.isBlank(authToken)) {
+            strings = request.getHeaders().get("access_token");
+            if (strings != null) {
+                authToken = strings.get(0);
+            }
+        }
+
+        return authToken;
+    }
+
 }
